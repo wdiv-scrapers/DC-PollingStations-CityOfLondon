75ce5102a3573b0b1eebb24e67fad0366ae91408
chris48s <chris.shaw480@gmail.com>
2017-06-27 11:59:45 +0100

    account for schema changes

--- scraper.py
+++ scraper.py
@@ -3,16 +3,16 @@ from dc_base_scrapers.xml_scraper import GmlScraper
 
 stations_url = "http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&version=1.1.0&service=wfs&typeNames=INSPIRE:UK_Parliamentary_General_Election_Polling_Places"
 stations_fields = {
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}OBJECTID': 'OBJECTID',
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}Address': 'Address',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}OBJECTID': 'OBJECTID',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}Address': 'Address',
 }
 
 districts_url = "http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&version=1.1.0&service=wfs&typeNames=INSPIRE:UK_Parliamentary_General_Election_Polling_Districts"
 districts_fields = {
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}OBJECTID': 'OBJECTID',
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}POLLING_DISTRICT': 'POLLING_DISTRICT',
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}GLA_CONSTITUENCY': 'GLA_CONSTITUENCY',
-    '{http://localhost:6080/arcgis/services/INSPIRE/MapServer/WFSServer}UK_PARLIAMENTARY_CONSTITUENCY': 'UK_PARLIAMENTARY_CONSTITUENCY',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}OBJECTID': 'OBJECTID',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}POLLING_DISTRICT': 'POLLING_DISTRICT',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}GLA_CONSTITUENCY': 'GLA_CONSTITUENCY',
+    '{http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer}UK_PARLIAMENTARY_CONSTITUENCY': 'UK_PARLIAMENTARY_CONSTITUENCY',
 }
 
 council_id = 'E09000001'
